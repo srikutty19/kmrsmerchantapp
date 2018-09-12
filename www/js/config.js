034ceb545ee4771e897751540857f392f07bb343
srinivasan <sri9132@gmail.com>
2018-09-12 01:03:08 -0700

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://kdboys.chiarro.com/merchantapp/api",	
+	'DialogDefaultTitle' : "KDB MERCHANT",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'APIHasKey' : "ef65011e2b65508fa173e1549e56d1e8"
+};
