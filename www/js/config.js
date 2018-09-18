a97fc6733f6ed6454506392edb051ffa1865ff93
srikutty19 <sri9132@gmail.com>
2018-09-18 11:53:17 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -2,6 +2,6 @@
 var krms_config ={			
 	'ApiUrl' : "http://kdboys.chiarro.com/merchantapp/api",	
 	'DialogDefaultTitle' : "KDB MERCHANT",
-	'pushNotificationSenderid' : "447235178837",
+	'pushNotificationSenderid' : "465251440510",
 	'APIHasKey' : "f9f7f58859171a96caccf20b05dffe35"
 };
